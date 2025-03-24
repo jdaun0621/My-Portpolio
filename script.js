3f7860aff7925cb23034e2a3bbe1e1e136cd9931
jdaun0621 <455a4555786@gmail.com>
2025-03-25 08:32:25 +0900

    commit

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
     const swiper = new Swiper(".swiper", {
-        direction: "vertical", // 세로 스와이프
+        direction: "vertical", 
         loop: false,
         speed: 800,
-        mousewheel: true, // 마우스 스크롤 가능
+        mousewheel: true, 
         keyboard: {
-            enabled: true, // 키보드 화살표 이동 가능
+            enabled: true, 
             onlyInViewport: true,
         },
         pagination: {
             el: ".swiper-pagination",
-            clickable: true, // 점 클릭 시 이동
+            clickable: true, 
             renderBullet: function (index, className) {
-                return `<span class="${className}"></span>`; // 숫자 제거
+                return `<span class="${className}"></span>`; 
             }
         },
         on: {
             slideChange: function () {
                 const footer = document.querySelector("footer");
-                if (swiper.activeIndex === 3) { // PROJECT 페이지 (data-slide="3")일 때만 보이기
+                if (swiper.activeIndex === 3) { 
                     footer.style.display = "block";
                 } else {
                     footer.style.display = "none";
